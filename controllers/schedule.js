a1c7f272c93493ab48d7703fae2971a7aabe315f
ConquerorFY <ryanrlfy33@gmail.com>
2023-03-24 15:43:55 +0800

    Added Daily Schedule API Functionalities

---
+++ controllers/schedule.js
@@ -0,0 +1,46 @@
+import { insert, updateOne, readAll } from "../db/helpers.js";
+
+// {
+//     "scheduleID": 1,
+//     "date": "2023-01-01",
+//     "workLocation": "flexi-hours",
+//     "workHours": "8am - 4pm",
+//     "workReport": "None",
+//     "supervisorComments": "Still under review",
+//     "employeeID": "e001"
+// }
+
+export const insertSchedule = async (req, res) => {
+    const scheduleData = req.body;
+    const results = await insert(scheduleData, "dailyschedule");
+    return res.status(200).json({ isSuccess: true, message: 'Daily Schedule has been inserted!' });
+}
+
+export const getSchedules = async (req, res) => {
+    const supvID = req.body.employeeID;
+    let employees = await readAll("employee");
+    employees = employees.filter((e) => e.supvID === supvID).map((e) => e.employeeID);
+    let schedules = await readAll("dailyschedule");
+    schedules = schedules.filter((s) => employees.indexOf(s.employeeID) > -1);
+    return res.status(200).json(schedules);
+}
+
+export const getScheduleBasedOnDate = async (req, res) => {
+    const scheduleDate = req.body.date;
+    const empId = req.body.employeeID;
+    const results = await readAll("dailyschedule");
+    for (let schedule of results) {
+        if (schedule.date === scheduleDate && schedule.employeeID === empId) {
+            return res.status(200).json({ isFound: true, data: schedule });
+        }
+    }
+    return res.status(200).json({ isFound: false, data: {} });
+}
+
+export const updateSchedule = async (req, res) => {
+    const scheduleId = req.body.scheduleID;
+    const newScheduleData = req.body;
+
+    const scheduleResult = await updateOne("scheduleID", scheduleId, newScheduleData, "dailyschedule");
+    return res.status(200).json({ isUpdated: true, data: scheduleResult });
+}
--- routes/schedule.js
+++ routes/schedule.js
@@ -1,4 +1,11 @@
 import express from "express";
+import { getScheduleBasedOnDate, getSchedules, insertSchedule, updateSchedule } from "../controllers/schedule.js";
 
 const router = express.Router();
+
+router.post("/insertSchedule", insertSchedule);
+router.post("/getSchedule", getScheduleBasedOnDate);
+router.post("/getSchedules", getSchedules);
+router.post("/updateSchedule", updateSchedule);
+
 export default router;
