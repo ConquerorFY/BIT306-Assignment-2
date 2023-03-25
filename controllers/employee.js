c276d0f6ffaf3ab2d98f264b2fb1c2756deaef5f
ConquerorFY <ryanrlfy33@gmail.com>
2023-03-25 11:55:34 +0800

    Minor Fixed & Adjustments

--- controllers/employee.js
+++ controllers/employee.js
@@ -14,7 +14,7 @@ import bcrypt from "bcryptjs";
 
 export const registerAccount = async (req, res) => {
     const existingAccount = await read("employeeID", req.body.employeeID, "employee");
-    if (existingAccount.length !== 0) return res.status(409).json("Account already exists!");
+    if (existingAccount.length !== 0) return res.status(200).json({ isSucceed: false, message: "Account already exists!" });
 
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(req.body.password, salt);
@@ -24,28 +24,33 @@ export const registerAccount = async (req, res) => {
         "password": hash
     }
     const result = await insert(accountData, "employee");
-    res.send(result).status(204);
+    res.status(200).json({ isSucceed: true, message: "Registration success!!" });
 }
 
 export const login = async (req, res) => {
     const targetUsers = await read("employeeID", req.body.employeeID, "employee");
-    if (targetUsers.length === 0) return res.status(404).json("Employee not found!");
+    if (targetUsers.length === 0) return res.status(200).json({ isSucceed: false, message: "Employee not found!" });
 
     const targetUser = targetUsers[0];
     const isPasswordCorrect = bcrypt.compareSync(
         req.body.password,
         targetUser.password
     )
-    if (!isPasswordCorrect) return res.status(400).json("Wrong employee ID or password!");
-    return res.status(200).json({
-        employeeID: targetUser.employeeID,
-        name: targetUser.name,
-        email: targetUser.email,
-        position: targetUser.position,
-        supvID: targetUser.supvID,
-        deptID: targetUser.deptID,
-        FWAStatus: targetUser.FWAStatus
-    })
+    if (!isPasswordCorrect) return res.status(200).json({ isSucceed: false, message: "Wrong employee ID or password!" });
+    return res.status(200).json(
+        {
+            isSucceed: true,
+            message: "Login success!!",
+            data: {
+                employeeID: targetUser.employeeID,
+                name: targetUser.name,
+                email: targetUser.email,
+                position: targetUser.position,
+                supvID: targetUser.supvID,
+                deptID: targetUser.deptID,
+                FWAStatus: targetUser.FWAStatus
+            }
+        });
 }
 
 export const logout = async (req, res) => {
